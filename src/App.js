0b728ca1b43ed425d6b6b4b8b0476d254cc680d5
Crystian Guzman <crystianguzman55@gmail.com>
2024-01-20 01:50:11 -0800

    Modified the image tag to fit right syntax and correctly declared them in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
 import logo from './logo.svg';
-// import hwlogo from '../public/images/hwlogo.png'
-// import grandprixcars from '../images/grandprixcars.png'
-// import manycars from '../images/manycars.png'
+import hwlogo from './images/hwlogo.png'
+import grandprixcars from './images/grandprixcars.png'
+import manycars from './images/manycars.png'
 import './App.css';
 
 function App() {
   return (
     <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a 
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="mt-5 p-4 bg-dark text-white text-center">
-        <p>Footer</p>
+      <>
+  <div className="container-sm">
+    <img src= {hwlogo} alt="hwlogo" width="10%" height={80} />
+  </div>
+  <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div className="container-fluid">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">
+            Active
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#">
+            Disabled
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div className="container mt-5">
+    <div className="row">
+      <div className="col-sm-4">
+        <div className="image-with-text">
+          <img
+            src={hwlogo}
+            alt="hotwheels logo"
+            width="100%"
+            height="100%"
+          />
         </div>
-        {/* <img src={hwlogo} className="Hotwheels-logo" alt="hwlogo" /> */}
+        <p>
+          <i>The hotwheels logo made 1968.</i>
+        </p>
+        <h3 className="mt-4">Some Links</h3>
+        <p>Lorem ipsum dolor sit ame.</p>
+        <ul className="nav nav-pills flex-column">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
+              Active
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <hr className="d-sm-none" />
+      </div>
+      <div className="col-sm-8">
+        <h2>Hotwheel series</h2>
+        <div className="img-with-text">
+          <img
+            src={grandprixcars}
+            alt="grandprixcars"
+            width="100%"
+            height={400}
+          />
+        </div>
+        <p>
+          <i>Hotwheel sets started in 1968.</i>
+        </p>
+        <p>
+          Some Hotwheel series overtime were Grand Prix, Hot Birds, Hot Line,
+          and Ultra Hots. The Grand Prix contained 8 cars and came out 1969.
+          Both Ferrari 312p and Porsche 917 cars were added in 1970 to the
+          series. The Ford J-Car was added over from 1968 to this series. Grand
+          Prix cars were The Ford Mark IV(1969), Ferrari 312P(1970), Lotus
+          Turbine(1969),Brabham RepcoF1(1969),Chapparal 2G(1969), Indy
+          Eagle(1969),Ford J-Car(1968),Lola GT70(1969),Lotus Turbine(1969),
+          McLaren M6A(1969),Porsche 917(1970), and Shelby Turbine(1969)
+        </p>
+      </div>
+    </div>
+  </div>
+</>
+        <div className="mt-5 p-4 bg-dark text-white text-center">
+          <p className='small'>©️ 2024
+          HotWheelsInfo</p>
+        </div>
+        
+        
       </header>
 
 
@@ -32,75 +119,6 @@ function App() {
 
    
         );
-{//     <div class="container-sm">
-//   <img src="../public/images/hwlogo.png" alt="hwlogo"  width="10%" height ="80">
-//  </div>
-
-// <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-//   <div class="container-fluid">
-//     <ul class="navbar-nav">
-//       <li class="nav-item">
-//         <a class="nav-link active" href="#">Active</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link" href="#">Link</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link" href="#">Link</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link disabled" href="#">Disabled</a>
-//       </li>
-//     </ul>
-//   </div>
-// </nav>
-
-// <div class="container mt-5">
-//   <div class="row">
-//     <div class="col-sm-4">
-//       <div class="image-with-text"><img src="../images/hwlogo.png" alt="hotwheels logo" width="100%" height="100%"></div>
-//       <p><i>The hotwheels logo made 1968.</i></p>
-//       <h3 class="mt-4">Some Links</h3>
-//       <p>Lorem ipsum dolor sit ame.</p>
-//       <ul class="nav nav-pills flex-column">
-//         <li class="nav-item">
-//           <a class="nav-link active" href="#">Active</a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#">Link</a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#">Link</a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link disabled" href="#">Disabled</a>
-//         </li>
-//       </ul>
-//       <hr class="d-sm-none">
-//     </div>
-//     <div class="col-sm-8">
-//       <h2>Hotwheel series</h2>
-//       <div class="img-with-text"><img src="../images/grandprixcars.png" alt="grandprixcars" width="100%" height="400"></div>
-//       <p><i>Hotwheel sets started in 1968.</i></p>
-//       <p>Some Hotwheel series overtime were Grand Prix, Hot Birds, Hot Line, and Ultra Hots. 
-//         The Grand Prix contained 8 cars and came out 1969. Both Ferrari 312p and Porsche 917 
-//         cars were added in 1970 to the series.
-//         The Ford J-Car was added over from 1968 to this series.
-//         Grand Prix cars were The Ford Mark IV(1969), Ferrari 312P(1970),
-//         Lotus Turbine(1969),Brabham RepcoF1(1969),Chapparal 2G(1969),
-//         Indy Eagle(1969),Ford J-Car(1968),Lola GT70(1969),Lotus Turbine(1969),
-//         McLaren M6A(1969),Porsche 917(1970), and Shelby Turbine(1969)</p>
-
-      
-//     </div>
-//   </div>
-// </div>
-//     <div className="App">
-}
-
-    
-
-
 }
   
 
