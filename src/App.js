70efe82987efff997bd88694f7599616bea528b3
Crystian Guzman <crystianguzman55@gmail.com>
2024-01-19 21:04:06 -0800

    modified index.html file, App.css file, and App.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
+// import hwlogo from '../public/images/hwlogo.png'
+// import grandprixcars from '../images/grandprixcars.png'
+// import manycars from '../images/manycars.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,9 +19,90 @@ function App() {
         >
           Learn React
         </a>
+        <div className="mt-5 p-4 bg-dark text-white text-center">
+        <p>Footer</p>
+        </div>
+        {/* <img src={hwlogo} className="Hotwheels-logo" alt="hwlogo" /> */}
       </header>
-    </div>
-  );
+
+
+
+
+
+
+   
+        );
+{//     <div class="container-sm">
+//   <img src="../public/images/hwlogo.png" alt="hwlogo"  width="10%" height ="80">
+//  </div>
+
+// <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+//   <div class="container-fluid">
+//     <ul class="navbar-nav">
+//       <li class="nav-item">
+//         <a class="nav-link active" href="#">Active</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="#">Link</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="#">Link</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link disabled" href="#">Disabled</a>
+//       </li>
+//     </ul>
+//   </div>
+// </nav>
+
+// <div class="container mt-5">
+//   <div class="row">
+//     <div class="col-sm-4">
+//       <div class="image-with-text"><img src="../images/hwlogo.png" alt="hotwheels logo" width="100%" height="100%"></div>
+//       <p><i>The hotwheels logo made 1968.</i></p>
+//       <h3 class="mt-4">Some Links</h3>
+//       <p>Lorem ipsum dolor sit ame.</p>
+//       <ul class="nav nav-pills flex-column">
+//         <li class="nav-item">
+//           <a class="nav-link active" href="#">Active</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">Link</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#">Link</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link disabled" href="#">Disabled</a>
+//         </li>
+//       </ul>
+//       <hr class="d-sm-none">
+//     </div>
+//     <div class="col-sm-8">
+//       <h2>Hotwheel series</h2>
+//       <div class="img-with-text"><img src="../images/grandprixcars.png" alt="grandprixcars" width="100%" height="400"></div>
+//       <p><i>Hotwheel sets started in 1968.</i></p>
+//       <p>Some Hotwheel series overtime were Grand Prix, Hot Birds, Hot Line, and Ultra Hots. 
+//         The Grand Prix contained 8 cars and came out 1969. Both Ferrari 312p and Porsche 917 
+//         cars were added in 1970 to the series.
+//         The Ford J-Car was added over from 1968 to this series.
+//         Grand Prix cars were The Ford Mark IV(1969), Ferrari 312P(1970),
+//         Lotus Turbine(1969),Brabham RepcoF1(1969),Chapparal 2G(1969),
+//         Indy Eagle(1969),Ford J-Car(1968),Lola GT70(1969),Lotus Turbine(1969),
+//         McLaren M6A(1969),Porsche 917(1970), and Shelby Turbine(1969)</p>
+
+      
+//     </div>
+//   </div>
+// </div>
+//     <div className="App">
 }
+
+    
+
+
+}
+  
+
 
 export default App;
